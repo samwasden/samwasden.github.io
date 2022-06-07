7a0b4597f6e3357b57464974232b7bbf45483ee9
Sam Wasden <samwasden@outlook.com>
2022-06-07 12:17:47 -0600

    rewrote css for mobile and desktop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function initialize() {
-    let menu = document.querySelector("#menu")
+    let menu = document.querySelector(".menu")
     menu.children[0].classList.add("current")
     document.addEventListener("scroll", setMenu)
     setMenuButtons()
@@ -11,7 +11,7 @@ function initializeMobile() {
 }
 
 function setMenu() {
-    let menu = document.querySelector("#menu")
+    let menu = document.querySelector(".menu")
     let position = Math.floor(window.scrollY/window.innerHeight)
     let bar = document.querySelector("section")
     let pos = Math.floor((window.scrollY*100)/(window.innerHeight*3))
@@ -27,7 +27,7 @@ function setMenu() {
 }
 
 function setMenuButtons(mobile = 0) {
-    let navButtons = document.querySelector("#menu").children
+    let navButtons = document.querySelector(".menu").children
     for (let i=0; i<navButtons.length; i++) {
         navButtons[i].addEventListener("click", (e) => {
             let index = Array.prototype.indexOf.call(e.target.parentNode.children, e.target)
@@ -45,30 +45,6 @@ function setSubmitButton() {
 
     form.addEventListener('submit', submitForm)
 }
-
-function submitForm(e) {
-    console.log('hello')
-    e.preventDefault()
-    let name = document.querySelector('#userName').value || 'name'
-    let message = document.querySelector('#userMessage').value || 'message'
-    let company = document.querySelector('#userCompany').value || 'company'
-    let email = document.querySelector('#userEmail').value || 'email'
-    const ACTION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSeqc8yHHc5tiD0axwvozh3d9L1K-sf9OKOxPAmcNEGpoyHZqg/viewform?usp=sf_link'
-    const NAME_ID = 'entry.732343598';
-    const MESSAGE_ID = 'entry.263866717';
-    const COMPANY_ID = 'entry.1774717764';
-    const EMAIL_ID = 'entry.998396544';
-    const formData = new FormData();
-    formData.append(NAME_ID, name)
-    formData.append(MESSAGE_ID, message)
-    formData.append(COMPANY_ID, company)
-    formData.append(EMAIL_ID, email)
-
-    axios.post(ACTION_URL, formData)
-        .then(res => {console.log(res.data)})
-        .catch(err => { console.log(err)});
-}
-
 
 if (screen.width >= 768) {
     initialize()
